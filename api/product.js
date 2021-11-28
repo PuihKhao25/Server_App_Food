ffb46f821a2cd0d820cd75f245ce294f911043ac
nvsang260720 <nvsang.18it2@vku.udn.vn>
2021-11-28 15:27:51 +0700

    edit

--- api/product.js
+++ api/product.js
@@ -5,12 +5,10 @@ const verifyToken = require('../middleware/auth')
 const CategoryController = require('../controller/CategoryController')
 const ProductController = require('../controller/ProductController')
 
-router.get('/user',verifyToken, (req,res) =>{
-    res.json({post: 'my post'})
-})
+
 router.get('/get-product', ProductController.getProducts)
 
-router.post('/add-product',verifyToken, ProductController.addProducts)
+router.post('/add-product', ProductController.addProducts)
 
 router.post('/add-category', CategoryController.addCategory)
 
