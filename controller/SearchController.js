c57cf28f6242d2b5c7762009ac1796208ebe2462
nvsang260720 <nvsang.18it2@vku.udn.vn>
2021-12-09 23:36:05 +0700

    edit search

--- controller/SearchController.js
+++ controller/SearchController.js
@@ -5,7 +5,8 @@ const postKey = async(req, res) =>{
     const {key} = req.body
     console.log(key)
     try {
-        const  SearchProduct =  await Product.find({name: key})
+        const  SearchProduct =  await Product.find({name: {$regex: key , $options: '$i'}})
+        //const  SearchProduct =  await Product.find({ key})
         return res.json(SearchProduct)
     } catch (error) {
         console.log(error)
