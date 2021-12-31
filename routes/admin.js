24a06770438287d89d151356ff6c3e07cdb21d2e
nvsang260720 <nvsang.18it2@vku.udn.vn>
2021-12-31 20:46:04 +0700

    add frontend

--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,20 @@ const AdminController = require('../controller/Backend/AdminController')
 router.get('/', function(req, res, next) {
     res.render('admin/home', { title: 'Admin' });
 });
-router.get('/add', function(req, res, next) {
-    res.render('admin/addproduct', { title: 'Admin' });
+router.get('/add-product', function(req, res, next) {
+    res.render('admin/addProduct', { title: 'Admin' });
 });
-router.get('/product', function (req, res, next) {
-    res.render('admin/product', { title: 'Admin' });
+router.get('/list-product', function(req, res, next) {
+    res.render('admin/listProduct', { title: 'Admin' });
+});
+router.get('/edit-product', function(req, res, next) {
+    res.render('admin/editProduct', { title: 'Admin' });
+});
+router.get('/category', function(req, res, next) {
+    res.render('admin/category', { title: 'Admin' });
 });
 
-router.get('/admin/login', function(req, res, next) {
+router.get('/login', function(req, res, next) {
     res.render('login', { title: 'Login', layout : false });
 });
 
