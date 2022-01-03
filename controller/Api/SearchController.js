5e62d58d72c8e05fb06970cfcc390c2d6e237395
nvsang260720 <nvsang.18it2@vku.udn.vn>
2022-01-03 23:36:09 +0700

    edit api

--- controller/Api/SearchController.js
+++ controller/Api/SearchController.js
@@ -4,13 +4,15 @@ const Product = require('../../models/Product')
 const postKey = async(req, res) => {
     const { key } = req.body
     
-    if(key){
+    try {
         const SearchProduct = await Product.find({ name: { $regex: key, $options: '$i' } })
         //const  SearchProduct =  await Product.find({ key})
         return res.json({ success: true, product: SearchProduct })
-    }else{
-        return res.json({ success: false, message: 'server error' })
+    } catch (error) {
+        return res.json({ success: false })
     }
+       
+    
     
     
 
